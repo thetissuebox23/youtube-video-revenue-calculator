abfb5791aafff0e671511079868e2f499eef7599
thetissuebox23 <choihanson0404@gmail.com>
2026-01-31 22:53:25 +0800

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function totalRevenue(){
   if(document.getElementById("has-ads").checked){
-    document.getElementById("total-revenue").textContent = Math.floor(document.getElementById("views").value /1000) * 7 + Math.floor(document.getElementById("video-duration").value / 5) * 2 * 0.03;
+    document.getElementById("total-revenue").textContent = Math.floor(document.getElementById("views").value /1000) * 7 + Math.floor(document.getElementById("video-duration").value / 5) * 2 * 0.03 * document.getElementById("views").value;
   }
   else{
     document.getElementById("total-revenue").textContent = Math.floor(document.getElementById("views").value /1000) * 7;
